ae6f88d20a36b32726d0be2156beb2969b83a2cf
de-dust-enjoyer <hartigsimon@gmail.com>
2025-08-14 09:07:27 +0200

    added viking names and random name generation!

--- viking.py
+++ viking.py
@@ -1,6 +1,12 @@
 from constants import *
+import viking_names
 
 class Viking:
 	def __init__(self, name=None, attributes=None):
+		'''name format = (forename, surname)'''
 		if name:
-			self.name = name
+			self.forename, self.surname = name
+		else:
+			self.forename, self.surname = viking_names.generate_name(viking_names.names)
+		print(self.forename, self.surname)
+viking = Viking()
