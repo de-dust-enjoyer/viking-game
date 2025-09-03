c07e1882440b8732010a4209c38dcaf0dcb8527c
de-dust-enjoyer <hartigsimon@gmail.com>
2025-09-03 18:05:09 +0200

    idk

--- soldier.py
+++ soldier.py
@@ -2,7 +2,8 @@ from constants import *
 
 from base_classes.person import Person
 
+
 class Soldier(Person):
     def __init__(self, name=None, attr=None):
-        '''name format = (forename, surname), attribute format = {category: {attribute: {stat: value}}}'''
+        """name format = (forename, surname), attribute format = {category: {attribute: {stat: value}}}"""
         Person.__init__(self, "english", name, attr)
